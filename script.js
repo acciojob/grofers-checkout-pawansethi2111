beb54ba1330dd3bd331de13af76fff11734e0528
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-01-04 19:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let monthly = document.getElementByClassName('monthly');
 
-let prices = document.querySelectorAll('.price');
-let sum = 0
+
 const getSum = () => {
 //Add your code here
-	// for(let t of prices){
-	// 	sum+= +t.innerHTML;
-	// }
-
- monthly.innerHTML = 5;
-	console.log('ok')
+// let monthly = document.getElementByClassName('monthly');
+let td = document.createElement("td");
+let tr = document.createElement("tr");
+let table = document.querySelector("table tbody");
+let prices = document.querySelectorAll('.price');
+let sum = 0
+	for(let t of prices){
+		sum+= +t.innerHTML;
+	}
+ td.innerHTML = sum;
+	td.id = "ans";
+	tr.append(td);
+	table.append(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
